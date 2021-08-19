4b1a2e4a3cc6cfc3b3a01aaeb01de4e08a80d851
Praveen Venigalla <praveen.venigalla@bluwave-ai.com>
2021-08-18 20:44:02 -0400

    add axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://praveen-fserver.herokuapp.com",
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 });
